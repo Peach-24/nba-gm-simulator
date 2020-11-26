107f80aca8f8b6e94e093f6752038a1f218271e8
Sam Aki <sam.k.aki8@gmail.com>
2020-11-26 14:48:23 +0000

    landing page, animated basketball, enter name form, how to play, rules sections done

---
+++ src/CurrentRoster.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CurrentRoster = () => {
+  return (
+    <header id="currentRoster" class="rosters">
+      <h3>Current Roster:</h3>
+    </header>
+  );
+};
+
+export default CurrentRoster;
